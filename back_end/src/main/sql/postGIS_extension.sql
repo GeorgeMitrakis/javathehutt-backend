0113d285f3856eb4ebf78d6f420aa1f6d83988b7
Markos <markosiliakis@outlook.com>
2019-05-25 13:48:35 +0300

    postgis

---
+++ back_end/src/main/sql/postGIS_extension.sql
@@ -0,0 +1,6 @@
+create extension postgis;
+create extension postgis_topology;
+create extension fuzzystrmatch;
+create extension address_standardizer;
+create extension address_standardizer_data_us;
+create extension postgis_tiger_geocoder;
