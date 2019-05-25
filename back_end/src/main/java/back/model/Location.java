7e79d3d06c75ee298702df79a5b4a2e1ee4ec28a
Markos <markosiliakis@outlook.com>
2019-05-25 16:15:07 +0300

    Location object

--- back_end/src/main/java/back/model/Location.java
+++ back_end/src/main/java/back/model/Location.java
@@ -6,6 +6,28 @@ public class Location {
     private String cityname;
     private double cordX, cordY;   //TODO: make them compatible with GIS
 
-    //TODO: getters & setters
+    public void setCityname(String cityname) {
+        this.cityname = cityname;
+    }
 
+    public void setCordX(String cords) {
+
+        this.cordX = Integer.getInteger(cords.replace("POINT(", "").replace(")", "").split(" ")[0]);
+    }
+
+    public void setCordY(String cords) {
+        this.cordY = Integer.getInteger(cords.replace("POINT(", "").replace(")", "").split(" ")[1]);
+    }
+
+    public double getCordX() {
+        return cordX;
+    }
+
+    public double getCordY() {
+        return cordY;
+    }
+
+    public String getCityname() {
+        return cityname;
+    }
 }
