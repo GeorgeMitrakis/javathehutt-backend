3ed495ebb4d48ca70c5313dfd98d5580232a58e3
petros <petros.anagnoy@gmail.com>
2019-06-12 16:38:31 +0300

    -roomImage resource (for real this time)

---
+++ back_end/src/main/java/back/api/RoomImagesResource.java
@@ -0,0 +1,44 @@
+package back.api;
+
+import back.Exceptions.JTHException;
+import back.Exceptions.JTHInputException;
+import back.conf.Configuration;
+import back.data.ImageDAO;
+import back.model.Image;
+import back.util.ImgFetch;
+import back.util.JsonMapRepresentation;
+import org.restlet.representation.ByteArrayRepresentation;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ResourceException;
+import org.restlet.resource.ServerResource;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RoomImagesResource extends ServerResource {
+    private ImageDAO imageDAO = Configuration.getInstance().getImageDAO();
+
+    @Override
+    protected Representation get() throws ResourceException {
+        try {
+            long roomId;
+            try {
+                String roomIdStr = getQueryValue("roomId");
+                roomId = new Long(roomIdStr).longValue();
+            } catch (Exception e) {
+                throw new JTHInputException("No room id specified/ invalid room Id");
+            }
+            List<Long> ids = imageDAO.getRoomImageIds(roomId);
+            Map<String, Object> res = new HashMap<>();
+            res.put("ids", ids);
+            return JsonMapRepresentation.result(true,"here's your ids!", res );
+
+        }catch(JTHInputException e){
+            return JsonMapRepresentation.result(false,"something went wrong: " + e.getErrorMsg(), null);
+        }catch (Exception e){
+            e.printStackTrace();
+            return JsonMapRepresentation.result(false,"something went wrong", null);
+        }
+    }
+}
