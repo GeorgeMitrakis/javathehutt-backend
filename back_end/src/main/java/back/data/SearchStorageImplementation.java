d9520e1055b493ab3353130f6a1b8401dedb6382
petros <petros.anagnoy@gmail.com>
2019-06-29 22:07:19 +0300

    Example search

--- back_end/src/main/java/back/data/SearchStorageImplementation.java
+++ back_end/src/main/java/back/data/SearchStorageImplementation.java
@@ -6,13 +6,22 @@ import back.model.Transaction;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 
 
 public class SearchStorageImplementation implements SearchStorageAPI {
@@ -32,7 +41,7 @@ public class SearchStorageImplementation implements SearchStorageAPI {
     public SearchStorageImplementation(){
         try {
             client = new PreBuiltTransportClient(Settings.EMPTY)
-                    .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9200));
+                    .addTransportAddress(new TransportAddress(InetAddress.getByName("localhost"), 9300));
         } catch (UnknownHostException e) {
             e.printStackTrace();
             client = null;
@@ -49,7 +58,7 @@ public class SearchStorageImplementation implements SearchStorageAPI {
             GetResponse response = client.prepareGet("rooms", "_doc", "room").get();
             System.out.println(response.getSourceAsString());
         } catch( Exception e){
-            //e.printStackTrace();
+            e.printStackTrace();
             System.out.print("FAILED TEST (TODO FIX ELASTIC)");
         }
     }
@@ -71,7 +80,30 @@ public class SearchStorageImplementation implements SearchStorageAPI {
 
     @Override
     public List<Room> searchRooms(SearchConstraints constraints, int limit, int offset) {
-        // TODO
+        List<Map> res = new LinkedList<>();
+
+        BoolQueryBuilder B = QueryBuilders.boolQuery();
+        if(constraints.getWifi()){
+            B = B.must(QueryBuilders.matchQuery("wifi",true));
+        }
+
+        if(constraints.getShauna()){
+            B = B.must(QueryBuilders.matchQuery("shauna",true));
+        }
+
+        if(constraints.getShauna()){
+            B = B.must(QueryBuilders.matchQuery("pool",true));
+        }
+
+        SearchResponse response = client.prepareSearch("testindex")
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(B)
+                .setFrom(0/*offset*/).setSize(10/*limit*/).setExplain(true)
+                .get();
+        for(SearchHit h : response.getHits().getHits()){
+            res.add(h.getSourceAsMap());
+        }
+        System.out.println(res);
         return null;
     }
 
