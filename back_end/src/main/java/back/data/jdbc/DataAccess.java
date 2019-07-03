914253ed5ae4111efa29d06f76f22dd68c9818b4
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2019-07-04 00:10:36 +0300

    Cascade also Locations table

--- back_end/src/main/java/back/data/jdbc/DataAccess.java
+++ back_end/src/main/java/back/data/jdbc/DataAccess.java
@@ -567,6 +567,7 @@ public class DataAccess {
             jdbcTemplate.update("DELETE FROM ratings WHERE room_id = ?", roomId);
             jdbcTemplate.update("DELETE FROM favorites WHERE room_id = ?", roomId);
             jdbcTemplate.update("DELETE FROM img WHERE room_id = ?", roomId);
+            jdbcTemplate.update("DELETE FROM location WHERE id = (SELECT location_id FROM room WHERE id = ?)", roomId);
         } catch (Exception e){
             e.printStackTrace();
             throw new JTHDataBaseException();
